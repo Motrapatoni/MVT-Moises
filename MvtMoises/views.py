6ad55e7ac5e47e8d307727db1c094023e7ef4f04
Motrapatoni <moiseserp@gmail.com>
2022-10-06 00:15:10 -0300

    first commit

---
+++ MvtMoises/views.py
@@ -0,0 +1,19 @@
+from django.template import Template, Context, loader
+from django.http import HttpResponse
+from datetime import datetime
+from home.models import Person
+
+
+def create_person(request, name, lastname, age, email):
+    guy = Person(name=name, last_name=lastname, age=age, create=datetime.now(), mail=email)
+    guy.save()
+    template = loader.get_template('post_user.html')
+    document = template.render({'person': guy})
+    return HttpResponse(document)
+
+
+def get_persons(request):
+    guys = Person.objects.all()
+    template = loader.get_template('get_user.html')
+    document = template.render({'person': guys})
+    return HttpResponse(document)
